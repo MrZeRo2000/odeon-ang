4d671b8c3baf4bf5ed42def879fe2c4ff14e8529
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-01-29 13:53:38 +0200

    iss#166 Migrate to Android 19: set up border styles

--- src/app/primeng.preset.ts
+++ src/app/primeng.preset.ts
@@ -16,6 +16,84 @@ export const OdeonPreset = definePreset(Material, {
       800: '{sky.800}',
       900: '{sky.900}',
       950: '{sky.950}'
+    },
+    colorScheme: {
+      light: {
+        text: {
+          color: "{surface.700}",
+          hoverColor: "{surface.800}",
+          mutedColor: "{surface.500}",
+          hoverMutedColor: "{surface.600}"
+        },
+        content: {
+          borderColor: "{surface.200}"
+        }
+      },
+      dark: {
+        text: {
+          color: "{surface.0}",
+          hoverColor: "{surface.0}",
+          mutedColor: "{surface.400}",
+          hoverMutedColor: "{surface.300}"
+        },
+        content: {
+          borderColor: "{surface.700}"
+        }
+      }
+    }
+  },
+  components: {
+    menubar: {
+      colorScheme: {
+        light: {
+          root: {
+            background: "{surface.50}"
+          }
+        },
+        dark: {
+          root: {
+            background: "{surface.800}"
+          }
+        }
+      }
+    },
+    datatable: {
+      colorScheme: {
+        light: {
+          header: {
+            background: "{surface.50}",
+            borderWidth: "1px 0px 1px 0px",
+          },
+          headerCell: {
+            background: "{surface.50}",
+            hoverBackground: "{surface.100}",
+          },
+          footer: {
+            background: "{surface.50}",
+          },
+          footerCell: {
+            background: "{surface.50}",
+          },
+        },
+        dark: {
+          root: {
+            borderColor: "{surface.800}"
+          },
+          header: {
+            background: "{surface.800}",
+          },
+          headerCell: {
+            background: "{surface.800}",
+            hoverBackground: "{surface.700}",
+          },
+          footer: {
+            background: "{surface.800}",
+          },
+          footerCell: {
+            background: "{surface.800}",
+          }
+        }
+      }
     }
   }
 });
