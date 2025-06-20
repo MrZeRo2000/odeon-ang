d639a9231ed18783a77b81be0e4fef68cd2a6e4c
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-06-20 15:13:05 +0300

    Fixed tests

--- src/app/components/tag/tagged-form/tagged-form.component.spec.ts
+++ src/app/components/tag/tagged-form/tagged-form.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaggedFormComponent } from './tagged-form.component';
+import {TrackFormComponent} from "../../track/track-form/track-form.component";
+import {DialogModule} from "primeng/dialog";
+import {DataSourceModule} from "../../../data-source/data-source.module";
+import {ServiceModule} from "../../../service/service.module";
+import {ConfirmationService, MessageService} from "primeng/api";
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {provideHttpClientTesting} from "@angular/common/http/testing";
 
 describe('TaggedFormComponent', () => {
   let component: TaggedFormComponent;
@@ -8,7 +15,19 @@ describe('TaggedFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaggedFormComponent]
+      declarations: [TrackFormComponent],
+      imports: [
+        //library
+        DialogModule,
+        //custom
+        DataSourceModule,
+        ServiceModule],
+      providers: [
+        MessageService,
+        ConfirmationService,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ]
     })
     .compileComponents();
 
